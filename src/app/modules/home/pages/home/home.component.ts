47e6f318631e02ad8d61bd242318d62aab8ec25b
Felipe Gabriel <pinheiro_felipeg@yahoo.com.br>
2022-03-17 10:37:14 -0300

    Creating news grid

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -20,13 +20,13 @@ export class HomeComponent implements OnInit {
         'https://br.web.img2.acsta.net/r_654_368/newsv7/16/01/19/20/37/565613.jpg',
     },
     {
-      title: 'Seven samurai rules?',
+      title: 'Seven samurai',
       content: 'Some are saying that this movie sucks ass!!',
       image:
         'https://cinemacomrapadura.com.br/imagens/2020/08/20200827-os-sete-samurais-thumbnail.jpg',
     },
     {
-      title: 'Captain America is the best movie evah?',
+      title: 'Captain America',
       content: 'Marvel Rules, DC sucks!',
       image:
         'https://www.infinitynews.com.br/wp-content/uploads/2019/08/thumb-1920-484780.jpg',
@@ -43,7 +43,27 @@ export class HomeComponent implements OnInit {
     'https://i.ytimg.com/vi/vvSyhWKVCsQ/maxresdefault.jpg',
   ];
 
+  sliderNews = [];
+  mainNews1;
+  mainNews2;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createNews();
+  }
+
+  createNews(): void {
+    this.sliderNews = this.newsList.filter((_, i) => {
+      return i < this.newsList.length - 2;
+    });
+
+    this.mainNews1 = this.newsList.filter((_, i) => {
+      return this.newsList.length - 2 === i;
+    })[0];
+
+    this.mainNews2 = this.newsList.filter((_, i) => {
+      return this.newsList.length - 1 === i;
+    })[0];
+  }
 }
